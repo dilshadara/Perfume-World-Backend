0b76dfda5fb0ee352856f88f539ea55a8364207e
ADUNSW\z3530200 <d.ara@blackdog.org.au>
2022-05-04 22:11:51 +1000

    get find one

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,7 +31,17 @@ async function run(){
             const cursor = perfumeCollection.find(query);
             const perfumes=await cursor.toArray();
             res.send(perfumes);
+        });
+
+        app.get('/perfume/:id', async(req,res) =>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)};
+            // const cursor = perfumeCollection.findOne(query);
+            const perfume=await perfumeCollection.findOne(query);
+            res.send(perfume);
         })
+
+
     }
     finally{
 
